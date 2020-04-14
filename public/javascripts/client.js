453a02ed4e4d746d599c1242ec07f60bad059db4
teridon <teridon@gmail.com>
2020-04-14 16:14:50 -0400

    restructure a bit to use XHR events

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,45 +1,46 @@
-console.log('Client-side code running');
+//console.log('Client-side code running');
+
+const songinfo        = document.getElementById('songinfo')
+const playpauseButton = document.getElementById('playpauseButton');
+const nextbutton      = document.getElementById('prevButton');
+const prevbutton      = document.getElementById('nextButton');
+
+function addListeners(oReq) {
+  oReq.addEventListener('load', function () {
+    var resp = oReq.responseText;
+    var obj = JSON.parse(resp);
+    songinfo.innerHTML = "iTunes is " + obj.state + ": <br>" + obj.artist + "<br>" + obj.song;
+    //console.log('button text:' + obj.playpausetext)
+    playpauseButton.innerHTML = obj.playpausetext;
+    //updatePlaypauseButton(obj.playpausetext);
+  });
+  oReq.addEventListener('timeout', function () {});
+  oReq.addEventListener('error', function () {});
+  oReq.addEventListener('abort', function () {});
+}
+
 
 function processRequest(button, e) {
     //console.log('processing request: ' + button);
     var oReq = new XMLHttpRequest();
-    //oReq.addEventListener("load", reqListener);
-    //oReq.responseType = "document";
+    addListeners(oReq);
     oReq.open("GET", "/" + button);
-    oReq.onreadystatechange = function () {
-      // In local files, status is 0 upon success in Mozilla Firefox
-      if(oReq.readyState === XMLHttpRequest.DONE) {
-        var status = oReq.status;
-        if (status === 0 || (status >= 200 && status < 400)) {
-          // The request has been completed successfully
-          var resp = oReq.responseText
-          //console.log('response text: ' + resp);
-          var obj = JSON.parse(resp);
-          document.getElementById('songinfo').innerHTML = "iTunes is " + obj.state + ": <p>" + obj.artist + "<br>" + obj.song;
-          //console.log('button text:' + obj.playpausetext)
-          document.getElementById('playpauseButton').innerHTML = obj.playpausetext;
-        } else {
-          //console.log('errors in response')
-        }
-      }
-    };
     oReq.send();
 }
 
 var intervalID = setInterval( function() {processRequest('status');}, 5000);
 
-const nextbutton = document.getElementById('prevButton');
 nextbutton.addEventListener('click', function() {
     processRequest ('prev');
 });
 
-const prevbutton = document.getElementById('nextButton');
+
 prevbutton.addEventListener('click', function(e) {
     processRequest ('next');
 });
 
-const playpausebutton = document.getElementById('playpauseButton');
-playpausebutton.addEventListener('click', function(e) {
+
+playpauseButton.addEventListener('click', function(e) {
     processRequest ('playpause');
 });
 
